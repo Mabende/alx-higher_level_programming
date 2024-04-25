c43ac850e0620d15398defd5ba803498765181fb
Mabende <belindamabende@gmail.com>
2024-04-25 00:10:37 -0700

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,4 +1,14 @@
 #!/usr/bin/node
-const myLetter = process.argv[2];
-if(isNaN(myLetter)){console.log('Missing size');}
-else{ for(let i = 0; i < myLetter; i++){console.log('X'.repeat(myLetter));}}
+const size = parseInt(process.argv[2], 10);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
