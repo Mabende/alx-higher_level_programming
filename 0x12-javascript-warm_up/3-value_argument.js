e79e22193db4c17bfcc377a61afc27a7fd388586
Mabende <belindamabende@gmail.com>
2024-04-25 00:03:28 -0700

    Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-const myArg = process.argv[2];
-if(myArg === undefined){console.log('No argument');}
-else{console.log(myArg)}
+const arg = process.argv[2];
+
+if (!arg) {
+  console.log('No argument');
+} else {
+  console.log(arg);
+}
