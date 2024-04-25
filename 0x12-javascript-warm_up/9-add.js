c42e967197831b4240cd8a575fc1a5155373c482
Mabende <belindamabende@gmail.com>
2024-04-25 00:12:40 -0700

    a script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-const a = process.argv[2];
-const b = process.argv[3];
-function add (a, b){
-	if(isNaN(a) || isNaN(b)){return (NaN);}
-	else{return(parseInt(a) + parseInt(b));}
+
+function add (a, b) {
+  return parseInt(a, 10) + parseInt(b, 10);
 }
-console.log(add(a, b));
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+console.log(add(arg1, arg2));
