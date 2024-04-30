9ef588e0f7efd10159b54c6a542aa450f777917c
Mabende <belindamabende@gmail.com>
2024-04-29 17:07:47 -0700

    script that takes in an argument and displays

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
 """
-    a script that takes in an argument and displays all values
+script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
 """
 
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
-        sys.exit(1)
+if __name__ == '__main__':
+    """
+    Access the database and get the states
+    """
 
-    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-    cursor = db.cursor()
-
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
-    cursor.execute(query)
-
-    rows = cursor.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(argv[4]))
+    rows = cur.fetchall()
 
     for row in rows:
         print(row)
-
-    cursor.close()
-    db.close()
